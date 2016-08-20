c9ae8eed7a99fe872a20fa0b496e47027f61b8f8
CharlesAnglin <charlie.anglin@gmail.com>
2016-08-20 22:47:08 +0100

    finished exercise 2.1

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,5 +1,26 @@
 object main extends App with week1 with week2 {
 
-  println(countChange(4, List(1,2)))
+//  println(countChange(4, List(1,2)))
+
+  val set4 = singletonSet(4)
+  val set5 = singletonSet(5)
+  val set6 = singletonSet(6)
+  val union56 = union(set5,set6)
+  val int5 = intersection(union56, set5)
+  val dif5 = diff(set4, union56)
+
+  def filter5(int: Int) = {
+    if(int == 5){
+      false
+    } else {
+      true
+    }
+  }
+
+  val filtered = filter(union56, filter5)
+
+  println(contains(filtered, 4))
+  println(contains(filtered, 5))
+  println(contains(filtered, 6))
 
 }
--- src/main/scala/week2.scala
+++ src/main/scala/week2.scala
@@ -4,17 +4,67 @@ trait week2 {
 
   def contains(s: Set, elem: Int): Boolean = s(elem)
 
-  def evenInts(int : Int) = {
-    if(int % 2 == 0){
-      true
-    } else {
-      false
+
+
+  def singletonSet(int: Int) : Set = {
+    def sinSet(input: Int) = {
+      if(input == int){
+        true
+      } else {
+        false
+      }
     }
+
+    sinSet
   }
 
+  def union (setA: Set, setB: Set) : Set = {
+    def returnSet (input: Int) = {
+      if(contains(setA, input) || contains(setB, input)){
+        true
+      } else {
+        false
+      }
+    }
 
-  def singletonSet(int: Int) = {
-    ???
+    returnSet
   }
+
+  def intersection (setA: Set, setB: Set) : Set = {
+    def returnSet (input: Int) = {
+      if(contains(setA, input) && contains(setB, input)){
+        true
+      } else {
+        false
+      }
+    }
+
+    returnSet
+  }
+
+  def diff (setA: Set, setB: Set) : Set = {
+    def returnSet(input: Int) = {
+      if(contains(setA, input) && !contains(setB, input)){
+        true
+      } else {
+        false
+      }
+    }
+
+    returnSet
+  }
+
+  def filter (set: Set, p: Int => Boolean) : Set = {
+    def returnSet(input: Int) = {
+      if(set(input)){
+        p(input)
+      } else {
+        false
+      }
+    }
+
+    returnSet
+  }
+
 
 }
